641fc5abff8decc44f2e1dc9381cf3200336a8cc
nsakuma <nsakuma@4cast.co.jp>
2015-12-25 16:44:34 +0900

    修正

--- template/js/main.js
+++ template/js/main.js
@@ -1,10 +1,10 @@
 // do something
 $(document).ready(function(){
-  $('.bxslider').bxSlider({
-  	auto: true,
-  	controls: false,
-  	pager: false,
-		speed: 1200
-  });
-  // $('header .bxslider li:not(:first)').delay(2000).css('display','block');
+	$('.bxslider').bxSlider({
+	auto: true,
+	controls: false,
+	pager: false,
+	speed: 1200,
+	onSlideAfter: function (){slider.startAuto();}
+	});
 });
